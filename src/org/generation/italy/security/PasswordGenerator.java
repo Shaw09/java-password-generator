f1c595fa3571f9125496c71d6bede2ddaf56264d
Shaw <michele_aliffi@yahoo.it>
2021-11-15 14:25:48 +0100

    Finish

--- src/org/generation/italy/security/PasswordGenerator.java
+++ src/org/generation/italy/security/PasswordGenerator.java
@@ -3,7 +3,16 @@ package org.generation.italy.security;
 public class PasswordGenerator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String firstname = "Pinco";
+		String lastname = "Pallo";
+		String prefcolor = "Magenta";
+		int dateday = 12;
+		int datemonth = 5;
+		int dateyear = 1994;
+		int dateborn = dateday + datemonth + dateyear;
+		
+		System.out.println("Pinco Pallo, nato il 12/05/1994, il cui colore preferito è il magenta");
+		System.out.print(firstname + "-" + lastname + "-" + prefcolor + "-" + dateborn);		
 
 	}
 
